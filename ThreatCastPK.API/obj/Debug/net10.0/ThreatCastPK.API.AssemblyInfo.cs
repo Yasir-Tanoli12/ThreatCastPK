e16dafc87a7bb8c50d8bbcfc6ad53cc910925bd6
Abdul Haadi <haadi.baloch.2004@gmail.com>
2026-05-31 23:50:15 +0500

    Haadi: CRUD 6 and 7 complete - Alert Subscriptions create view update delete toggle

--- ThreatCastPK.API/obj/Debug/net10.0/ThreatCastPK.API.AssemblyInfo.cs
+++ ThreatCastPK.API/obj/Debug/net10.0/ThreatCastPK.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThreatCastPK.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc8ceb3710b63d40b839dc0960657820f3c65717")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dffad88ddbbdfb94e7f7b61d653a66447ca7471")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThreatCastPK.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThreatCastPK.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
